c0e80b906857e257ce99821f8e506f9df881d7a9
Tanish-shetty <tanishshetty425@gmail.com>
2025-01-18 16:31:00 +0530

    added Add Desgination page

---
+++ Admin page/Add_desgination/add_desgination.js
@@ -0,0 +1,21 @@
+function addDesignation() {
+    let designation = prompt("Please enter the Designation:");
+    if (designation) {
+        let designationList = document.getElementById('designationList');
+        let newDesignation = document.createElement('p');
+        newDesignation.textContent = designation;
+
+        let deleteIcon = document.createElement('img');
+        deleteIcon.src = 'delete.png'; // Path to your delete icon
+        deleteIcon.alt = 'Delete';
+        deleteIcon.onclick = function () {
+            designationList.removeChild(newDesignation);
+        };
+
+        newDesignation.appendChild(deleteIcon);
+        designationList.appendChild(newDesignation);
+    }
+}
+    document.querySelector('.toggle-button').addEventListener('click', function() {
+        document.getElementById('sidebar').classList.toggle('expanded');
+    });
